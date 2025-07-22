382ab6aab5802c0e0b2ab9cd23250984a0e5452b
sentore847 <dmytro.grygorchuk@amedia.ua>
2025-07-22 23:33:48 +0300

    add projectRoute

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
+import projectRoutes from './routes/project.routes';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is working 🚀');
