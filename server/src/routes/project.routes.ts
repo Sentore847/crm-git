bb08bf341083f8473813ea2e84f363814e4f7b5d
sentore847 <dmytro.grygorchuk@amedia.ua>
2025-07-23 11:55:17 +0300

    getProjects

--- server/src/routes/project.routes.ts
+++ server/src/routes/project.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { addProject } from '../controllers/project.controller';
+import { addProject, getProjects } from '../controllers/project.controller';
 import {authenticate} from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.post('/', authenticate, addProject);
+router.post('/', authenticate, getProjects);
+
 
 export default router;
