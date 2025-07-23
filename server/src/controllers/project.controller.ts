7d35d2cd56b5f4b400b87943f71beb5708cce878
sentore847 <dmytro.grygorchuk@amedia.ua>
2025-07-23 12:12:45 +0300

    Delete request import (unused)

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { prisma } from "../utils/prisma"
 import { AuthRequest } from '../middlewares/auth.middleware';
 import axios from 'axios';
