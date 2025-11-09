dc24e04b73689194d652302909620060e1a240d7
Dmytro Grygorchuk <dmytro.grygorchuk@amedia.ua>
2025-11-09 20:15:05 +0200

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
